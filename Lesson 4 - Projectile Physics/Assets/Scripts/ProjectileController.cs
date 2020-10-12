42e42aa725f4c247af795b75d5dac422f3694c50
unknown <christopher.a.cruzen@gmail.com>
2020-10-12 12:42:46 -0500

    Upload Lesson 4

---
+++ Lesson 4 - Projectile Physics/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+
+	/*--- Veriables ---*/
+
+	private float time;
+	private float deleteTime = 10f;
+
+
+	/*--- Methods ---*/
+
+    void Update() {
+        
+        // Update Time
+        time += Time.deltaTime;
+
+        // Conditionally KYS
+        if (time > deleteTime) {
+        	Destroy(gameObject);
+        }
+    }
+}
---
+++ Lesson 4 - Projectile Physics/Assets/Scripts/ShootController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+
+
+	/*--- Variables ---*/
+
+	public GameObject projectile;
+	public Transform camera;
+	public float speed = 10f;
+
+	private float time = 0f;
+	private float shootDelay = 0.2f;
+	private float lastShoot = 0f;
+
+
+
+	/*--- Methods ---*/
+
+    void Update() {
+
+    	// Update Time
+    	time += Time.deltaTime;
+     
+    	// Get Fire Input
+    	float shoot = Input.GetAxis("Fire1");
+
+    	// Shoot Projectile
+    	if (shoot == 1f && time > lastShoot + shootDelay) {
+
+    		// Fire Projectile
+    		GameObject newProjectile = Instantiate(projectile, camera.transform.position + (camera.transform.forward * 2f), Quaternion.identity);
+
+    		// Give Projectile Velocity
+    		(newProjectile.GetComponent(typeof(Rigidbody)) as Rigidbody).velocity = camera.transform.forward * speed;
+
+    		// Update Last Shoot
+    		lastShoot = time;
+    	}
+
+    }
+}
