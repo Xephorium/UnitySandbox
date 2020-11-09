56df463c21ae3d1a95de02d0c5dad015a9f58596
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2020-11-08 21:56:55 -0600

    Update FPS Platform

---
+++ Sandbox/Assets/Scripts/Animation/ElevatorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class ElevatorController : MonoBehaviour {
+
+
+	/*--- Variables ---*/
+
+	private float currentTime;
+	private float cycleFactor;
+	private Vector3 initialPosition;
+
+    public float cycleLength;
+    public float riseHeight;
+
+
+    /*--- Lifecycle Methods ---*/
+
+
+    void Start() {
+    	initialPosition = transform.position;
+    }
+ 
+    void Update () {
+
+    	// Update Time
+        currentTime += Time.deltaTime;
+        cycleFactor = currentTime % cycleLength / cycleLength;
+
+        // Update Rotation
+        transform.position = new Vector3(
+        	initialPosition.x,
+        	initialPosition.y + Mathf.Abs(0.5f - cycleFactor) * (riseHeight * 2f),
+        	initialPosition.z
+        );
+    }
+}
